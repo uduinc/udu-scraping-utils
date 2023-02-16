20d4c7a83920168c39cc58ed8eff6326beea230f
Terry Rogers <terry.rogers@udu.nu>
2023-02-16 14:12:51 -0500

    Fix bad semi's

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const scrapingUtils = {
 		});
 
 		return uri;
-	};
+	},
 	getDomain: function getDomain (url, uriObj) {
 		const secondLevelExtensions = ["com","net","co","gov","edu","org","mil","io","biz","go","hub","business","weebly","squarespace","blogspot","yolasite","wordpress","wixsite","wix","vpweb","ueniweb","webs","tripod","negocio","godaddysites","homestead","jimdo","us"];
 
@@ -56,7 +56,7 @@ const scrapingUtils = {
 		}
 
 		return domain.toLowerCase();
-	};
+	}
 };
 
 exports = module.exports = scrapingUtils;
